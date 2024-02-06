0bb0387049b943b2d9b91b07869354bad95abe24
BAWORID <fazzafabianahmad@my.id>
2024-02-06 12:44:26 +0700

    Update and rename run.sh to ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Ngrok command
+ngrok_cmd="ngrok tunnel --label edge=edghts_2bypB13WUY8dEK3gnfcEbu4pXFL http://localhost:443 &"
+
+# Main loop
+while true; do
+    $ngrok_cmd
+    echo "Connected"
+    # Sleep for 5 minutes
+    sleep 1800
+done
