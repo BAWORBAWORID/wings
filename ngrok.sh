7b2333ad0bb141032cb460e80d64c1ea2e894e7c
BAWORID <fazzafabianahmad@my.id>
2024-05-08 07:20:18 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -3,10 +3,4 @@
 # Ngrok command
 ngrok_cmd="ngrok tunnel --label edge=edghts_2bypB13WUY8dEK3gnfcEbu4pXFL http://localhost:443 > /dev/null 2>&1"
 
-# Main loop
-while true; do
-    $ngrok_cmd
-    echo "Connected"
-    # Sleep for 5 minutes
-    sleep 1800
-done
+$ngrok_cmd
